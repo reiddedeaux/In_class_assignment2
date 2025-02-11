c4a5c83fa7f610644322022a8905a4d47f7e5f86
reiddedeaux <rdedeaux@usf.edu>
2025-02-11 14:33:50 -0500

    Task 1 and Task 2

---
+++ cc_example.js
@@ -0,0 +1,25 @@
+// task 1 working with arrays
+let products = [
+    {name: "Laptop", price: 1200, category: "Electronics"},
+    {name: "Phone", price: 800, category: "Electronics",},
+    {name: "Desk Chair", price: 150, category: "Furniture"},
+    {name: "Headphones", price: 100, category: "Electronics"},
+    {name: "Notebooks", price: 5, category: "Stationery"}
+];
+
+
+function getProductByCategory(products, category){
+   return products.filter (product => product.category === category)
+}
+
+console.log(getProductByCategory(products, "Electronics"));
+
+// Task 2
+function applyDiscount(products, discountRate){
+    return products.map(product =>({
+        ...product,
+        price: product.price - (product.price * discountRate)
+    }
+))
+}
+console.log(applyDiscount(products,.1));
